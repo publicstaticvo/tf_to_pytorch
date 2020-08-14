e83b86e79c432ab0f6ceda1de6872ea60217396b
余天枢 <yts17@mails.tsinghua.edu.cn>
2020-08-14 16:04:29 +0800

    file_utils

--- convert_gpt2_original_tf_checkpoint_to_pytorch.py
+++ convert_gpt2_original_tf_checkpoint_to_pytorch.py
@@ -21,10 +21,9 @@ from io import open
 
 import torch
 
-from transformers import (CONFIG_NAME, WEIGHTS_NAME,
-                                                     GPT2Config,
-                                                     GPT2Model,
-                                                     load_tf_weights_in_gpt2)
+from transformers.file_utils import CONFIG_NAME, WEIGHTS_NAME
+from transformers.modeling_gpt2 import GPT2Model, load_tf_weights_in_gpt2
+from transformers.configuration_gpt2 import GPT2Config
 
 import logging
 logging.basicConfig(level=logging.INFO)
